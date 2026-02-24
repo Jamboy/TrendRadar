a86372e00844270339116d23e8fe90b7a0a40538
Jambooy <1140142559@qq.com>
2026-02-24 11:09:02 +0800

    update 6.0

---
+++ .history/trendradar/__init___20260224105300.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+"""
+TrendRadar - 热点新闻聚合与分析工具
+
+使用方式:
+  python -m trendradar        # 模块执行
+  trendradar                  # 安装后执行
+"""
+
+from trendradar.context import AppContext
+
+<<<<<<< HEAD
+__version__ = "5.5.2"
+=======
+__version__ = "6.0.0"
+>>>>>>> 3a1452eb249f10a8e00aa44ab7d77e9530b495c0
+__all__ = ["AppContext", "__version__"]
